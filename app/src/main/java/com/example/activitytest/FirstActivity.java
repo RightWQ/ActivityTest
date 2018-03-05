ace07ef29325a35ce9b0b3ea44540f0e4cbd5d91
RightWQ <1747058726@qq.com>
2018-03-05 10:16:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -0,0 +1,16 @@
+// 创建第一个活动
+
+package com.example.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+    }
+}
